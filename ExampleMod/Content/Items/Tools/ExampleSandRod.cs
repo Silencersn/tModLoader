b540d9f858092a9a98230bdb15d3cdea2450ae8f
JavidPack <javidpack@gmail.com>
2024-11-18 14:18:37 -0700

    Simplify some logic, code style.

--- ExampleMod/Content/Items/Tools/ExampleSandRod.cs
+++ ExampleMod/Content/Items/Tools/ExampleSandRod.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -8,17 +7,19 @@ namespace ExampleMod.Content.Items.Tools
 {
 	// ExampleSandRod is a sand version of Dirt Rod
 	// It can be used to move different sand blocks (including ExampleSand) around
+	// To implement a tile relocating rod for other types of blocks, you'll need to make a custom projectile for them
 	public class ExampleSandRod : ModItem
 	{
 		public override void SetStaticDefaults() {
 			ItemID.Sets.DuplicationMenuToolsFilter[Type] = true;
+			ItemID.Sets.GamepadWholeScreenUseRange[Type] = true;
 		}
 
 		public override void SetDefaults() {
 			Item.width = 28;
 			Item.height = 28;
 
-			// Copy from Dirt Rod
+			// Copied from Dirt Rod
 			Item.channel = true;
 			Item.knockBack = 5f;
 			Item.useStyle = ItemUseStyleID.Swing;
@@ -31,66 +32,53 @@ namespace ExampleMod.Content.Items.Tools
 		}
 
 		public override bool CanUseItem(Player player) {
-			if (player.whoAmI != Main.myPlayer)
-				return false;
+			if (player.whoAmI != Main.myPlayer) {
+				return true;
+			}
 
 			// Calculate the tile position where the mouse is on
-			Point tilePos = (Main.MouseScreen + Main.screenPosition).ToTileCoordinates();
-			if (player.gravDir == -1f) {
-				// If the screen is reversed, it is need to modify the Y of position
-				tilePos.Y = (int)((Main.screenPosition.Y + Main.screenHeight - Main.mouseY) / 16);
-			}
+			Point tilePos = Main.MouseWorld.ToTileCoordinates();
 			Tile tile = Main.tile[tilePos];
 
-			if (!tile.HasTile)
+			if (!tile.HasTile) {
 				return false;
+			}
 
 			// If the tile is not sand, the item will not be used
-			// This can be removed, if you want it not to move sand block only
-			if (!Main.tileSand[tile.TileType])
+			if (!Main.tileSand[tile.TileType]) {
 				return false;
+			}
 
 			// Get which projectile the tile will create when falling
-			if (TileID.Sets.FallingBlockProjectile[tile.TileType] is not TileID.Sets.FallingBlockProjectileInfo data)
+			if (TileID.Sets.FallingBlockProjectile[tile.TileType] is not TileID.Sets.FallingBlockProjectileInfo data) {
 				return false;
+			}
 
-			// Try to kill the tile whithout item dropping
+			// Try to kill the tile without item dropping
 			WorldGen.KillTile(tilePos.X, tilePos.Y, noItem: true);
 
-			// Fail to kill tile
-			if (Main.tile[tilePos].HasTile)
+			// If for some reason the tile can't be killed, don't use the item
+			if (Main.tile[tilePos].HasTile) {
 				return false;
+			}
 
-			// If theh tile was kill successfully
-			// set Item.shoot to data.FallingProjectileType to create the corresponding projectil of the killed tile
+			// If the tile was killed successfully,
+			// set Item.shoot to data.FallingProjectileType to create the projectile corresponding to the killed tile
 			Item.shoot = data.FallingProjectileType;
 
-			// If it is on the multiplayer client, sync it to the server
+			// If it is on the multiplayer client, sync the tile destruction to the server
 			if (Main.netMode == NetmodeID.MultiplayerClient) {
+				// 4 corresponds to the KillTileNoItem message
 				NetMessage.SendData(MessageID.TileManipulation, number: 4, number2: tilePos.X, number3: tilePos.Y);
 			}
-			
+
 			return true;
 		}
 
-		public override bool CanShoot(Player player) {
-			// Do not shoot if Item.shoot is ProjectileID.None
-			return Item.shoot != ProjectileID.None;
-		}
-
 		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
-			// Make sure the spwan position of projectile is the same as mouse
-			position = Main.MouseScreen + Main.screenPosition;
-			if (player.gravDir == -1f) {
-				position.Y = Main.screenPosition.Y + Main.screenHeight - Main.mouseY;
-			}
+			// Make sure the spawn position of projectile is the same as mouse
+			position = Main.MouseWorld;
 			player.LimitPointToPlayerReachableArea(ref position);
-		}
-
-		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
-			// Reset Item.shoot
-			Item.shoot = ProjectileID.None;
-			return base.Shoot(player, source, position, velocity, type, damage, knockback);
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
